1b02a7a78389dea45cdc8c1f4f21e6d824baee98
Niccolo Canestrari <niccolo.canestrari@gmail.com>
2016-09-23 08:40:32 +0200

    rm copyright

--- Sources/UTBuilder/Include/globber.h
+++ Sources/UTBuilder/Include/globber.h
@@ -1,11 +1,3 @@
-/* @owner ... */
-/**
- * @file globber.h
- * @brief This module declares 
- *
- * @copyright Copyright of this program is the property of AMADEUS, without
- * whose written permission reproduction in whole or in part is prohibited.
- */
 
 #ifndef __GLOBBER_H__
 #define __GLOBBER_H__
